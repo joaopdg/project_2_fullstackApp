dff892c63eb85bfc1d55f1ec51779cb775b4978c
joaoGon <jpgon@live.com.pt>
2022-05-16 16:19:11 +0100

    commentes first try

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const commentSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
 
   post: { type: Schema.Types.ObjectId, ref: "Post" },
 
--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -5,3 +5,25 @@ const User = require("../models/User.model");
 const Post = require("../models/Post.model");
 const Comment = require("../models/Comment.model");
 
+router.post('/ad-details/:id', (req, res, next) => {
+    const { id } = req.params;
+    const { author, content } = req.body;
+  
+        Post.findById(id)
+          .then((post) => {
+            let newComment;
+  
+              newComment = new Comment({ author: req.session.user._id, content });
+            
+  
+            newComment.save().then((savedComment) => {
+              post.comments.push(savedComment._id);
+  
+              post.save().then((updatedPost) => res.redirect(`/ad-details/${updatedPost._id}`));
+            });
+          })
+          .catch((err) => next(err));
+      });
+
+
+  module.exports = router;
