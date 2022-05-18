e4172939306bd5a7032de7264ed3aaf8f22c6afb
Mariana Oliveira <marianalucas018@gmail.com>
2022-05-18 17:39:01 +0100

    jquery code

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,21 @@ document.addEventListener(
   },
   false
 );
+
+$(document).ready(function () {
+  $("#notificationLink").click(function () {
+    $("#notificationContainer").fadeToggle(300);
+    $("#notification_count").fadeOut("slow");
+    return false;
+  });
+
+  //Document Click hiding the popup
+  $(document).click(function () {
+    $("#notificationContainer").hide();
+  });
+
+  //Popup on click
+  $("#notificationContainer").click(function () {
+    return false;
+  });
+});
